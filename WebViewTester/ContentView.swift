c2d455b03e2c134bfa67914255b7f9da5281fda0
Mark Blandford <mark.blandford@landg.com>
2020-03-03 15:29:04 +0000

    Initial Commit

---
+++ WebViewTester/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WebViewTester
+//
+//  Created by Mark Blandford on 03/03/2020.
+//  Copyright © 2020 L&G. All rights reserved.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello World")
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
